b3911c1af9c5fee9973256040e02a879adc7bded
stucottam-maker <stucottam@gmail.com>
2026-06-25 22:18:21 +0100

    Update email contact from hello@crudelia.mx to hola@crudelia.co.uk

--- src/App.jsx
+++ src/App.jsx
@@ -146,7 +146,7 @@ function Footer() {
 
       <div>
         <p>Bookings & catering</p>
-        <a href="mailto:hello@crudelia.mx">hello@crudelia.mx</a>
+        <a href="mailto:hello@crudelia.mx">hola@crudelia.co.uk</a>
         <br />
         <a
           href="https://www.instagram.com/crudelia.mx/"
